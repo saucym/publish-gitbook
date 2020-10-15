2043605e79c5bf1f53378364db36cfeebdd123bf
saucym <qhc_m@qq.com>
2020-10-15 11:28:06 +0800

    Update publish.sh

--- bin/publish.sh
+++ bin/publish.sh
@@ -1,20 +1,11 @@
 #!/bin/sh
 
+git clone https://github.com/saucym/saucym.github.io.git _book
+
 # install the plugins and build the static site
 gitbook install && gitbook build
 
-# checkout to the gh-pages branch
-git checkout gh-pages || git checkout -b gh-pages
-
-# pull the latest updates
-git pull origin gh-pages --rebase
-
-# copy the static site files into the current directory.
-cp -R _book/* .
-
-# remove 'node_modules' and '_book' directory
-git clean -fx node_modules
-git clean -fx _book
+cd _book
 
 # add all files
 git add .
@@ -24,7 +15,4 @@ COMMIT_MESSAGE="Update gitbook `date '+%Y-%m-%d %H:%M:%S'`"
 git commit -a -m "${COMMIT_MESSAGE}"
 
 # push to the origin
-git push -u origin gh-pages
-
-# checkout to the master branch
-git checkout master
+git push
